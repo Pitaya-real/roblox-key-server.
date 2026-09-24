339e6a2c90f32602b95a56206d4d3cbdf9ac691b
Pitaya-real <pitayauisupport@gmail.com>
2026-09-24 14:09:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -191,7 +191,7 @@ app.get('/getkey', async (req, res) => {
             <html lang="vi">
             <head>
                 <meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Dragon Fruit Key System</title>
+                <title>Pitaya Key System</title>
                 <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;700;800&display=swap" rel="stylesheet">
                 <style>${COMMON_STYLE}</style>
             </head>
